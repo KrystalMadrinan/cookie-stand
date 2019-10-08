b78a7033f52a251db13ffc082296493de20f414c
KRYSTAL MADRINAN <KRYSTAL.MADRINAN@GMAIL.COM>
2019-10-08 00:48:53 -0700

    added tokyo and dubai

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 var parentEl = document.getElementById('parentElement');
-// parentEl.textContent = 'attempting to contact html.';
 
 var child = document.createElement('h1');
 child.textContent = 'Seattle: ';
@@ -15,7 +14,7 @@ var seattle = {
   avgCookie: 6.3,
   sumRandNum: 0,
 
-  hourTotal: function() {
+  hourTotal: function () {
     var randNum = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust * this.avgCookie);
     this.sumRandNum += randNum;
     return randNum;
@@ -23,10 +22,9 @@ var seattle = {
   },
 
 
-  render: function() {
+  render: function () {
     for (var i = 0; i < this.shopHours.length; i++) {
       var childEl = document.createElement('li');
-      // childEl.textContent = 'Shop Hours: ' + this.shopHours[i];
       childEl.textContent = `${this.shopHours[i]}:  ${this.hourTotal()} cookies`;
       parentEl.appendChild(childEl);
     }
@@ -37,3 +35,69 @@ var seattle = {
 
 seattle.render();
 seattle.hourTotal();
+
+
+var tokyoChild = document.createElement('h1');
+tokyoChild.textContent = 'Tokyo: ';
+parentEl.appendChild(tokyoChild);
+
+var tokyo = {
+  shopHours,
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+  sumRandNum: 0,
+
+  hourTotal: function() {
+    var randNum = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust * this.avgCookie);
+    this.sumRandNum += randNum;
+    return randNum;
+  },
+
+  render: function() {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var childEl = document.createElement('li');
+      childEl.textContent = `${this.shopHours[i]}:  ${this.hourTotal()} cookies`;
+      parentEl.appendChild(childEl);
+    }
+    childEl.textContent = `Total: ${this.sumRandNum} cookies`;
+    parentEl.appendChild(childEl);
+  },
+};
+
+tokyo.render();
+tokyo.hourTotal();
+
+
+var dubaiChild = document.createElement('h1');
+dubaiChild.textContent = 'Dubai: ';
+parentEl.appendChild(dubaiChild);
+
+var dubai = {
+  shopHours,
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+  sumRandNum: 0,
+
+  hourTotal: function() {
+    var randNum = Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust * this.avgCookie);
+    this.sumRandNum += randNum;
+    return randNum;
+  },
+
+  render: function() {
+    for (var i = 0; i < this.shopHours.length; i++) {
+      var childEl = document.createElement('li');
+      childEl.textContent = `${this.shopHours[i]}: ${this.hourTotal()} cookies`;
+      parentEl.appendChild(childEl);
+    }
+    childEl.textContent = `Total: ${this.sumRandNum} cookies`;
+    parentEl.appendChild(childEl);
+  },
+};
+
+dubai.render();
+dubai.hourTotal();
+
+
